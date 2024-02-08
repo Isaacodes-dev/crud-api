752cc9e5ab00b431a6bc9a3722daf38572c74134
raphkayo <kayomboraphaely@gmail.com>
2024-02-08 20:23:30 +0200

    corrected end point name

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('posts', PostCOntroller::class);
+Route::apiResource('post', PostCOntroller::class);
